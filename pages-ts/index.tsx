9fd15547aacf444cb34f5d3e41cf60dfcc9268c5
rchaser53 <tayoshizawa29@gmail.com>
2017-11-05 23:07:50 +0900

    set typescript for next.js

---
+++ pages-ts/index.tsx
@@ -0,0 +1,2 @@
+import * as React from 'react'
+export default () => <div>Welcome to next.js!</div>
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,4 @@
-import React from 'react'
-export default () => <div>Welcome to next.js!</div>
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+exports.default = () => React.createElement("div", null, "Welcome to next.js!");
